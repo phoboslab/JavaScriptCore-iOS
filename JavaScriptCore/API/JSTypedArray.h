69f4ef19e546b0bd9945301e467418051389f8ec
Dominic Szablewski <mail@phoboslab.org>
2015-10-30 17:53:23 +0100

    Documentation

--- JavaScriptCore/API/JSTypedArray.h
+++ JavaScriptCore/API/JSTypedArray.h
@@ -41,7 +41,7 @@ typedef enum {
 @abstract           Returns a JavaScript value's Typed Array type
 @param ctx          The execution context to use.
 @param value        The JSValue whose Typed Array type you want to obtain.
-@result             A value of type JSTypedArrayType that identifies value's Typed Array type.
+@result             A value of type JSTypedArrayType that identifies value's Typed Array type, or kJSTypedArrayTypeNone if the object is not a Typed Array.
 */
 JS_EXPORT JSTypedArrayType JSObjectGetTypedArrayType(JSContextRef ctx, JSObjectRef object);
 
@@ -55,23 +55,50 @@ JS_EXPORT JSTypedArrayType JSObjectGetTypedArrayType(JSContextRef ctx, JSObjectR
 */
 JS_EXPORT JSObjectRef JSObjectMakeTypedArray(JSContextRef ctx, JSTypedArrayType arrayType, size_t numElements);
 
-
+/*! @typedef JSDataRef A Typed Array data buffer. */
 typedef struct OpaqueJSData* JSDataRef;
-
-JS_EXPORT void JSDataRetain(JSDataRef data);
-JS_EXPORT void JSDataRelease(JSDataRef data);
-JS_EXPORT void * JSDataGetBytesPtr(JSDataRef data);
-JS_EXPORT size_t JSDataGetLength(JSDataRef data);
 
 /*!
 @function
-@abstract           Returns a pointer to a Typed Array's data in memory
+@abstract           Returns a retained JSDataRef that encapsulates a pointer to a Typed Array's data in memory
 @param ctx          The execution context to use.
-@param value        The JSValue whose Typed Array type data pointer you want to obtain.
-@param byteLength   A pointer to a size_t in which to store the byte length of the Typed Array
-@result             A pointer to the Typed Array's data or NULL if the JSValue is not a Typed Array.
+@param value        The JSObjectRef whose Typed Array type data pointer you want to obtain.
+@result             A JSDataRef or NULL if the JSObjectRef is not a Typed Array. The return value is automatically retained and has to be released again with JSDataRelease
 */
 JS_EXPORT JSDataRef JSObjectGetRetainedTypedArrayData(JSContextRef ctx, JSObjectRef object);
+
+/*!
+@function
+@abstract         Retains a JavaScript data object.
+@param data       The JSData to retain.
+@result           A JSDataRef that is the same as data.
+*/
+JS_EXPORT JSDataRef JSDataRetain(JSDataRef data);
+
+/*!
+@function
+@abstract         Releases a JavaScript data object.
+@param data       The JSData to release.
+*/
+JS_EXPORT void JSDataRelease(JSDataRef data);
+
+/*!
+@function
+@abstract         Returns a pointer to the data buffer that serves as the backing store for a JavaScript data object.
+@param data       The JSData whose backing store you want to access.
+@result           A pointer to the raw data buffer that serves as data's backing store, which will be deallocated when the data is deallocated.
+*/
+JS_EXPORT void * JSDataGetBytesPtr(JSDataRef data);
+
+/*!
+@function
+@abstract         Returns the number of bytes in a JavaScript data object.
+@param data       The JSData whose length (in bytes) you want to know.
+@result           The number of bytes stored in the data object.
+*/
+JS_EXPORT size_t JSDataGetLength(JSDataRef data);
+
+
 
 
 #ifdef __cplusplus
